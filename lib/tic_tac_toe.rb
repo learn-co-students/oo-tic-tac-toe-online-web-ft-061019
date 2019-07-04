3247071cb86c0a8c67ca32dfdd7e68641be98bf8
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-04 07:34:00 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class TicTacToe
   
    WIN_COMBINATIONS = [
@@ -31,23 +32,21 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
-  def input_to_index(string_int_movement)
-    index = string_int_movement.to_i - 1
-    index
+  def input_to_index(string_int_move)
+    string_int_move.to_i - 1
   end 
   
-   def move(board, index)
-    board[index] = player
-    play(board)
-  end 
+   def move(index, player = "X" )
+    @board[index] = player
+    end 
   
-  def position_taken?(board, index)
-    board[index] == "X" || board[index] == "O"
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
   end
   
-  def valid_move?(board, index)
+  def valid_move?(index)
 
-  if index.between?(0,8) && !position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(index)
       puts 'this is a valid move'
     return true
   else
@@ -55,15 +54,15 @@ class TicTacToe
   end
 end
   
-  def turn(board)
+  def turn
     puts "Please enter 1-9:"
     user_input = gets.strip
     index = input_to_index(user_input)
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
-      display_board(board)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
     else 
-      turn(board)
+      turn
     end 
   end 
   
